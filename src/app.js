9259a14ad18a1cfe695940bd58a7818ad5182f07
aromero7764 <aromero7764@gmail.com>
2022-12-01 00:05:13 -0400

    add env.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const port = require('../config').api.port
 
 app.use(express.json())
 
+app.get('/', (req, res)=> {
+  res.status(200).json({message: 'OK!'})
+})
 
 app.listen(port, ()=> {
   console.log(`Server started at port ${port}`)
