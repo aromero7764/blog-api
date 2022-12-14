b9233d2c766f4a2ddfc1a4536ec2e2c78c29b0ff
Alberto Romero <aromero7764@gmail.com>
2022-12-13 22:26:29 -0400

    UTH

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,9 +1,27 @@
 // email 
 // password
+const { findUserByemail } = require('../users/users.controllers')
+const { comparePassword } = require('../utils/crypto')
 
-const checkUserCredential = (email, password) => {
+// esta funcion va a validar si los datos pertenecn o no a un usuario
+
+
+const checkUserCredential = async(email, password) => {
+
+    try {
+        const user = await findUserByemail(email)
+        const verifyPassword = comparePassword(password, user.password)
+        if(verifyPassword) {
+            return user
+        }
+        return null
+        
+    } catch (error) {
+        return null
+    }
     
 }
 
+module.exports = checkUserCredential
 
 
